5a502051f4452401c1de74b167ce26438a705dc1
Wilson Weets <wilson.weets@mic-belgique.be>
2018-05-14 16:55:26 +0200

    Adaptation to Anguler (Wip)

--- BbqMicBack/App_Start/WebApiConfig.cs
+++ BbqMicBack/App_Start/WebApiConfig.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using System.Web.Http.Cors;
 
 namespace BbqMicBack
 {
@@ -13,6 +14,8 @@ namespace BbqMicBack
 
             // Web API routes
             config.MapHttpAttributeRoutes();
+            var cors = new EnableCorsAttribute("*", "*", "*");
+            config.EnableCors();
 
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
